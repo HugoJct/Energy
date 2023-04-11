3ead6ac718e752bbce7f8baaa1e9a85ab20aeacf
Matthieu Le Franc <matt.lefranc@gmail.com>
2023-04-11 11:28:13 +0200

    le controleur il est mieux un peu presque

--- src/fr/jacototlefranc/energy/controller/SelectController.java
+++ src/fr/jacototlefranc/energy/controller/SelectController.java
@@ -12,22 +12,35 @@ import fr.jacototlefranc.energy.view.mainmenu.MainMenu;
 import fr.jacototlefranc.energy.view.mainmenu.SelectMenu;
 
 public class SelectController {
-    
+    private Frame frame;
+    private HashMap<String, Level> levels;
+    private PlayController playController;
+
     public SelectController(Frame frame, MainMenu menu, HashMap<String, Level> levels, SelectMenu select) {
+
+        this.frame = frame;
+        this.levels = levels;
 
         frame.getMenu().setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, KeyEvent.CTRL_DOWN_MASK));
         frame.getMenu().addActionListener(e -> {
-            frame.setPanel(menu);
-            //frame.getContentPane().removeMouseListener(pc);
+            if (playController != null) {
+                frame.getContentPane().removeMouseListener(playController);
+                frame.setPanel(menu);
+            }
         });
 
         int i = 0;
         for (String s : levels.keySet()) {
             select.getButton(i).addActionListener(e -> {
-                frame.setPanel(new BoardView(levels.get(s)));
-                frame.getContentPane().addMouseListener(new PlayController(levels.get(s)));
+                buttonClicked(s);
             });
             i++;
         }
+    }
+
+    private void buttonClicked(String s) {
+        playController = new PlayController(levels.get(s));
+        frame.setPanel(new BoardView(levels.get(s)));
+        frame.getContentPane().addMouseListener(playController);
     }
 }
